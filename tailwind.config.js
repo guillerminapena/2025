de68216d8442094d385a4498fb1ffbfd75cbd589
guillermina <128639290+guillerminapena@users.noreply.github.com>
2025-05-11 17:30:04 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                gotham: ['gotham', 'sans-serif'],
+            },
+        },
+    },
+};
