178dc8fa96011546b4337051df05587762e78c05
guillermina <128639290+guillerminapena@users.noreply.github.com>
2025-05-22 09:34:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function abrirPopup() {
+    document.getElementById('popup').classList.remove('hidden');
+}
+
+function cerrarPopup() {
+    document.getElementById('popup').classList.add('hidden');
+}
+
+var input_nombre = document.getElementById("nombre");
+
+
+console.log(input_nombre);
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+<script>
+  function mostrarPopup() {
+    document.getElementById("miModal").classList.remove("hidden");
+  }
+
+  function cerrarModal() {
+    document.getElementById("miModal").classList.add("hidden");
+  }
+</script>
